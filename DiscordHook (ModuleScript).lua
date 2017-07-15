2f05d71672ccf5a68a0c82cf6f4314f7441e95f1
HoodedBlack <josephdussault718@gmail.com>
2017-07-14 23:06:13 -0400

    Create DiscordHook (ModuleScript).lua

---
+++ DiscordHook (ModuleScript).lua
@@ -0,0 +1,18 @@
+ local HttpService = game:GetService("HttpService")
+ 
+local function HttpEnabled()
+    local s = pcall(function()
+        HttpService:GetAsync("https://www.google.com/")
+    end);
+    return s
+end;
+ 
+return function(url)
+    if HttpEnabled() then
+        return function(name, message, img)
+            HttpService:PostAsync(url, HttpService:JSONEncode({content = message})) -- (plr and "https://www.roblox.com/headshot-thumbnail/image?userId=".. plr.userId  .."&width=420&height=420&format=png" or "")
+        end;
+    end;
+end;
+
+--HoodedBlack
