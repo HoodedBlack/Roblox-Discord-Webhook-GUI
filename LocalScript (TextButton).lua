1e3359c8124cc1cdefdfd3a9770607b2e6b7e1f4
HoodedBlack <josephdussault718@gmail.com>
2017-07-14 23:10:15 -0400

    Update LocalScript (TextButton).lua

--- LocalScript (TextButton).lua
+++ LocalScript (TextButton).lua
@@ -1,8 +1,9 @@
-	local MessageEvent 				= game.ReplicatedStorage.Events.DiscordShoutEvent;
+local MessageEvent 				= game.ReplicatedStorage.Events.DiscordShoutEvent;
 local Hook 			= require(game.ReplicatedStorage.DiscordHook)("YOUR DISCORD WEBHOOK");
  
 script.Parent.MouseButton1Click:Connect(function()
     MessageEvent:FireServer(script.Parent.Parent.TextBox.Text)
 end);
+
 
 --HoodedBlack
